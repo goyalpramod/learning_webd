07b4c6271ace4fe7abbbaff6b78cf979a697ccd1
goyalpramod <goyalpramod1729@gmail.com>
2025-02-23 21:57:07 +0530

    finish till 1.12

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 
 const Button = ({ reviewValue, setReview, review}) => (
-  <button onClick={ () => setReview(reviewValue + 1) }>
+  <button onClick={ () => setReview(review => review + 1) }>
     {review}
   </button>
 )
@@ -11,41 +11,78 @@ const Statistics = ({good, neutral, bad}) => {
   const average = (good - bad) / all
   const positive = good / all * 100
 
-  if (all > 0) {
+  if (all === 0) {
     return (
-      <div>
-        <p>good {good}</p>
-        <p>neutral {neutral}</p>
-        <p>bad {bad}</p>
-        <p>all {all}</p>
-        <p>average {average}</p>
-        <p>positive {positive} %</p>
-      </div>
+      <tr>
+        <td colSpan="6">No feedback given</td>
+      </tr>
     )
   }
-  return (
-    <div>
-      <p>No feedback given</p>
-    </div>
-  )
 
+  return (
+    <>
+      <tr>
+        <td>good</td>
+        <td>{good}</td>
+      </tr>
+      <tr>
+        <td>neutral</td>
+        <td>{neutral}</td>
+      </tr>
+      <tr>
+        <td>bad</td>
+        <td>{bad}</td>
+        </tr>
+      <tr>
+        <td>all</td>
+        <td>{all}</td>
+        </tr>
+      <tr>
+        <td>average</td>
+        <td>{average.toFixed(1)}</td>
+        </tr>
+      <tr>
+        <td>positive</td>
+        <td>{positive.toFixed(1)}%</td>
+      </tr>
+      <tr>
+      </tr>
+    </>
+  )
+}
+
+const StaticLine = ({text, value, all}) => {
+  const averageScore = all === 0 ? 0 : value / all
+  
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{averageScore.toFixed(1)}</td>
+    </tr>
+  )
 }
 
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  
 
   return (
     <div>
       <h1>give feedback</h1>
       <Button reviewValue={good} setReview={setGood} review="good" />
       <Button reviewValue={neutral} setReview={setNeutral} review="neutral" />
-      <Button reviewValue={bad} setReview={setBad} review="bad" />
+      <Button reviewValue={bad} setReview={setBad} review="bad" />      
       <h1>statistics</h1>
-      <Statistics good={good} neutral={neutral} bad={bad}/>
+
+      <table>
+        <tbody>
+          <Statistics good={good} neutral={neutral} bad={bad}/>
+          <StaticLine text="good" value={good} all={good + neutral + bad} />
+          <StaticLine text="neutral" value={neutral} all={good + neutral + bad} />
+          <StaticLine text="bad" value={bad} all={good + neutral + bad} />
+        </tbody>
+      </table>
     </div>
   )
 }
