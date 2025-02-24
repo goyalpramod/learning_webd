ca1f16216ffeb6ba7bf2f71368c566d1ca94fa66
goyalpramod <goyalpramod1729@gmail.com>
2025-02-24 23:29:58 +0530

    add filter

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -2,8 +2,11 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('a new name....')
   const [newNumber, setNewNumber] = useState('a new number....')
   const handleNewName = (event) => {
@@ -27,10 +30,29 @@ const App = () => {
     setNewName('')
   }
 
+  const [searchName, setSearchName] = useState('')
+  const personsToShow = searchName === ''
+  ? []
+  : persons.filter(person => person.name.toLowerCase().includes(searchName.toLowerCase()))
+  const handleSearchName = (event) => {
+    setSearchName(event.target.value)
+  }
+  
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with: <input
+        value={searchName}
+        onChange={handleSearchName}
+        />
+        {personsToShow.map(person =>
+          <div key={person.name}>{person.name}</div>
+        )}
+      </div>
+
       <form>
+        <h3>Add a new person</h3>
         <div>
           name: <input 
           value={newName}
