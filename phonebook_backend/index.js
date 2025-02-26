02f2c1a0d519cac59eb56bd9b134903b70c2cb31
goyalpramod <goyalpramod1729@gmail.com>
2025-02-26 22:23:37 +0530

    adding phonebook backend

---
+++ phonebook_backend/index.js
@@ -0,0 +1,80 @@
+const express = require('express')
+const app = express()
+
+let notes = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(notes)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const note = notes.find(note => note.id === id)
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.get('/api/info', (request, response) => {
+  const date = new Date()
+  response.send(`<p>Phonebook has info for ${notes.length} people</p><p>${date}</p>`)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  notes = notes.filter(note => note.id !== id)
+  response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+    if (!body.name || !body.number) {
+        return response.status(400).json({ 
+        error: 'name or number missing' 
+        })
+    }
+    if (notes.find(note => note.name === body.name)) {
+        return response.status(400).json({ 
+        error: 'name must be unique' 
+        })
+    }
+    const note = {
+        name: body.name,
+        number: body.number,
+        id: Math.floor(Math.random() * 1000),
+    }
+    notes = notes.concat(note)
+    response.json(note)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
